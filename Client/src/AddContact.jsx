25db28831952555bc626eddacc78868ec82b30ff
Rishabh Sangal <sangalrishabh29@gmail.com>
2026-03-09 23:49:59 +0530

    Update AddContact.jsx

--- Client/src/AddContact.jsx
+++ Client/src/AddContact.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ToastContainer, toast,Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const AddContact = ({
+const AddCont act = ({
   handleModal,
   showModal,
   url,
